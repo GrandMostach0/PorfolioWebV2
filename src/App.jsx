ea550620692a003a8b5cdf3e9962727aa2ccd76a
GrandMostach0 <kreedlegend0@gmail.com>
2025-03-19 13:12:24 -0600

    fixed error background overflow

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ import Inicio from './components/Inicio'
 function App() {
   return(
     <>
-      <div class="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-      
+      <div class="absolute inset-0 z-[-2] bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+
       <div className="mx-w-[1200px] mx-auto">
         <NavBar />
         <main className='text-white'>
--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -9,7 +9,7 @@ import Row_Right_Down from "./icons/Row_Right_Down";
 
 function Inicio() {
     return(
-        <div className=" text-white flex items-center justify-center overflow-hidden">
+        <div className="border-1 flex  items-center justify-center">
             <div className=" mr-7 w-2xl py-4 px-4">
                 <h1 className="font-bold text-5xl mb-2">Desarrollador Frontend & Ciberseguridad</h1>
                 <p className="text-base">Soy un apasionado del desarrollo web y la ciberseguridad. Listo para explorar y crecer en esta apasionante travesía.</p>
@@ -30,8 +30,6 @@ function Inicio() {
             <div className="max-w-[15%] max-h-[20%] rounded-full overflow-hidden">
                 <img src={FotoPersonal} alt="Foto personal" className="object-cover w-full h-full" />
             </div>
-
-
         </div>
     );
 }
